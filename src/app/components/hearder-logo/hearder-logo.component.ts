e5e8bc7da90d78c45c83ba3eb91d35a3babb8a8c
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 16:34:54 -0300

    ✨ feat: header-logo component created

---
+++ src/app/components/hearder-logo/hearder-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hearder-logo',
+  templateUrl: './hearder-logo.component.html',
+  styleUrls: ['./hearder-logo.component.scss']
+})
+export class HearderLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
