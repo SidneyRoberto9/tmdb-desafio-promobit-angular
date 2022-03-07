4c312a1a170fad842065049bc8729585496cb8b4
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 18:15:00 -0300

    📦 refactor: clean details page code

--- src/app/components/detalhes components/detalhes/detalhes.component.ts
+++ src/app/components/detalhes components/detalhes/detalhes.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/model/movies';
 
 @Component({
   selector: 'app-detalhes',
   templateUrl: './detalhes.component.html',
-  styleUrls: ['./detalhes.component.scss']
+  styleUrls: ['./detalhes.component.scss'],
 })
 export class DetalhesComponent implements OnInit {
+  @Input() movie?: Movie;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  src = (path: string) => `https://image.tmdb.org/t/p/w500${path}`;
 }
