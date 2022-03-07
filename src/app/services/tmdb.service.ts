cf416cdb1b534af4179252ca100e367a99db0833
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 14:33:21 -0300

    ✨ feat: using enviroment api_key

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Root } from '../model/tmdb';
+import { environment } from '../../environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TmdbService {
+  Base = `https://api.themoviedb.org/3/movie/popular?api_key=${environment.API_KEY}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPopulares() {
+    this.http.get<Root>(this.Base).subscribe((data) => {
+      data.results.map((movie) => console.log(movie));
+    });
+  }
 }
