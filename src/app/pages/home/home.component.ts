3c4251f0f8ef34e95dd1c5d9c9447fefef7cff85
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 15:20:08 -0300

    🏗️ build: getPopulares variable to use in all aplication added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Result } from 'src/app/model/tmdb';
+import { TmdbService } from 'src/app/services/tmdb.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  populares$: Result[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private tmdb: TmdbService) {
+    this.populares$ = this.tmdb.getPopulares();
   }
 
+  ngOnInit(): void {}
 }
