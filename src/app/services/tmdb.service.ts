1a07de36f4ad5d1b866f5702a6fdee3f924b9f7a
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 17:37:49 -0300

    ✨ feat: all api return transform in observables returns and create get Movie

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -2,20 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Result, Root } from '../model/tmdb';
 import { environment } from '../../environments/environment';
+import { Movie } from '../model/movies';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class TmdbService {
-  Base = `https://api.themoviedb.org/3/movie/popular?api_key=${environment.API_KEY}`;
+  id = '';
 
   constructor(private http: HttpClient) {}
 
-  getPopulares(): Result[] {
-    let populares: Result[] = [];
-    this.http.get<Root>(this.Base).subscribe((data) => {
-      data.results.map((movie) => populares.push(movie));
-    });
+  getPopulares(): Observable<Root> {
+    const BASE_VIDEO = `https://api.themoviedb.org/3/movie/popular?api_key=${environment.API_KEY}`;
 
-    return populares;
+    return this.http.get<Root>(BASE_VIDEO);
+  }
+
+  getMovies(id: string): Observable<Movie> {
+    const BASE_MOVIE_DETAILS = `https://api.themoviedb.org/3/movie/${id}?api_key=${environment.API_KEY}`;
+
+    return this.http.get<Movie>(BASE_MOVIE_DETAILS);
   }
 }
