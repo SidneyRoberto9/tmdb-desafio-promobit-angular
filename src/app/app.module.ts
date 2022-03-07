892c6cc67b8c25dd506b4f0dd48eef04e289d0bc
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 13:08:35 -0300

    ✨ feat: display-card component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CardComponent } from './components/card/card.component';
+import { DisplayCardComponent } from './components/display-card/display-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    DisplayCardComponent
   ],
   imports: [
     BrowserModule,
