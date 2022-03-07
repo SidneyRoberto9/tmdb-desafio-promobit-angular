1290e8a8a179aa55a136c0a191beb0f3e6cbd26f
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 17:38:36 -0300

    ✨ feat: consume get movies

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/model/movies';
+import { TmdbService } from 'src/app/services/tmdb.service';
 
 @Component({
   selector: 'app-details',
@@ -8,9 +10,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
   id: string = '';
+  movie: Movie = {} as Movie;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private tmdb: TmdbService) {
     this.route.params.subscribe((params) => (this.id = params['id']));
+    this.tmdb.getMovies(this.id).subscribe((data) => (this.movie = data));
   }
 
   ngOnInit(): void {}
