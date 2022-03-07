7d86dcd5d301d1eff43c1bdad9c3a729af90fc54
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-07 11:56:40 -0300

    📦 refactor: remove base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'tmdb-desafio-promobit-angular';
-}
+export class AppComponent {}
