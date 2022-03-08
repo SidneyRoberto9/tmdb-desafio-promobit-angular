f0c1efcad2302bb16a1badabeb4f13214db669a8
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-08 18:30:11 -0300

    🏗️ build: patch full removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'detalhes/:id', component: DetailsComponent, pathMatch: 'full' },
+  { path: 'detalhes/:id', component: DetailsComponent },
 ];
 
 @NgModule({
