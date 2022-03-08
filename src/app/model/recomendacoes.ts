8f20eacf36ba3501453c8dabbc21a1359af11602
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-08 14:37:59 -0300

    ✨ feat: recomendacoes model created

---
+++ src/app/model/recomendacoes.ts
@@ -0,0 +1,32 @@
+export interface Recomendacoes {
+  page: number;
+  results: ResultRecomendacoes[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface ResultRecomendacoes {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  media_type: MediaType;
+  title: string;
+  original_language: OriginalLanguage;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: Date;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export enum MediaType {
+  Movie = 'movie',
+}
+
+export enum OriginalLanguage {
+  En = 'en',
+}
