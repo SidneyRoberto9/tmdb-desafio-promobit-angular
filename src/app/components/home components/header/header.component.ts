84dd0485dc9b18d23e1baa23a1701a6085d36bad
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-03-12 02:34:04 -0300

    ✨ feat: filter list function added

--- src/app/components/home components/header/header.component.ts
+++ src/app/components/home components/header/header.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { GenreFilter } from 'src/app/model/genres';
+import { genres as g } from './genres';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  active: boolean = false;
+  genres = g;
+  filterGenres: GenreFilter[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  filterGenre(id: number) {
+    if (this.inFilter(id)) {
+      this.filterGenres = this.filterGenres.filter((genre) => genre.id !== id);
+    } else {
+      this.genres.find((genre) => {
+        if (genre.id === id) {
+          this.filterGenres.push(genre);
+        }
+      });
+    }
   }
 
+  inFilter(id: number) {
+    return this.filterGenres.find((genre) => genre.id === id);
+  }
 }
